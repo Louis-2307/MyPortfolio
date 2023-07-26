f5881833b478e59a27558a00323c7d511e85cf55
Louis-2307 <Louis.le.2307@gmail.com>
2023-07-26 19:07:28 -0400

    Update

--- js/Typing.js
+++ js/Typing.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type", {
-    strings: ["Mobile Developer", "Network Engineer ", "Help-Desk Support"],
+    strings: ["Mobile Developer", "Network Engineer ", "Help-Desk Supporter"],
     typeSpeed: 50,
     backSpped: 50,
     loop: true
